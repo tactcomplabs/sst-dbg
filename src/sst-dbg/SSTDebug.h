68adc6b052dea3435416643a57d9ea89d7efcf7e
jleidel <john.leidel@gmail.com>
2022-08-25 14:16:15 -0500

    basic library functionality working; woot!

--- src/sst-dbg/SSTDebug.h
+++ src/sst-dbg/SSTDebug.h
@@ -15,12 +15,11 @@
 #include <string>
 #include <vector>
 #include <cstdarg>
-#include <typelist.hpp>
+#include <stdarg.h>
+#include <typeinfo>
 
 // -- Required Macros
 #define SSTCYCLE  uint64_t
-
-using namespace tl;
 
 class SSTDebug {
 private:
@@ -29,10 +28,30 @@ private:
   std::string Name;       ///< Name of the component
   std::string Path;       ///< Output path
 
-  /// SSTDebug: SST internal type derivation
+  std::ofstream Bin;      ///< Binary output
+
+  template<typename T>
+  void __internal_dump(){
+  }
+
   template<typename T>
   void __internal_dump(T v){
-    // register the variable 'v' of type 'T'
+    Bin << v << std::endl;
+  }
+
+  template<typename T, typename U, typename... Args>
+  bool __internal_dump(T t, U u, Args... args){
+    Bin << t << ":" << u << std::endl;
+  }
+
+  template<typename... Args>
+  void __internal_dump(Args... args){
+    //__internal_dump(args...);
+  }
+
+  template<typename T1, typename T2>
+  void __internal_dump(T1 v1, T2 v2){
+    Bin << v1 << ":" << v2 << std::endl;
   }
 
 public:
@@ -48,9 +67,17 @@ public:
   /// SSTDebug: SST Debug destructor
   ~SSTDebug();
 
-  /// SSTDebug: SST Debug data dump
-  template<typename T, typename... Args>
-  void dump(SSTCYCLE cycle, tl::type_list<> list, T first, Args... args){
+  /// SSTDebug: SST Debug data dump function
+  template<typename T, typename U, typename... Args>
+  bool dump(SSTCYCLE cycle, T t, U u, Args... args){
+
+    std::string BinName = Name + "." + std::to_string(cycle) + ".out";
+    Bin.open(BinName.c_str(), std::ios::out);
+
+    Bin << t << ":" << u << std::endl;
+    __internal_dump(args...);
+
+    Bin.close();
   }
 };
 
--- test/iface/iface_test1.cpp
+++ test/iface/iface_test1.cpp
@@ -12,46 +12,19 @@
 #include <string>
 #include "SSTDebug.h"
 
-#define _(...) __VA_ARGS__
-
-#define S_CHECK_EQ(type1, type2)                                               \
-    static_assert(std::is_same<type1, type2>::value, #type1 " == " #type2)
-
 std::string TEST_NAME = "iface_test1";
 
 int main( int argc, char **argv ){
   std::cout << "TEST = " << TEST_NAME << std::endl;
 
-  SSTDebug Dbg;
+  SSTDebug Dbg(TEST_NAME,"./");
 
   std::string FOO = TEST_NAME;
   unsigned BAR    = 42;
   uint64_t CYCLE  = 100;
 
-  using T0 = type_list<>;
-  using T0_1 = T0::append<type_list<std::string>>;
-  using T0_2 = T0_1::append<type_list<unsigned>>;
-
-  S_CHECK_EQ(T0::prepend<bool>, _(type_list<bool>));
-
-  T0 t0;
-  T0_2 t1;
-
-  Dbg.dump(CYCLE, t0, FOO, BAR);    // this compiles
-
-  // This does not compile
-  // How do we make the TypeList variadic to record the type info?
-  //
-  // Issue: Variadic templates are handled recursively.  We can derive
-  //        the type info from the variadic template arguments _OR_
-  //        do something with the data members, but not both.
-  //
-  // Goal:
-  //       I want to do the following in the `dump` method:
-  //       1) Record the type info and data sizes from each of the data arguments
-  //       2) Write a header out to a binary file describing the data format
-  //       3) Write all the data to a binary file in the prescribed format
-  //Dbg.dump(CYCLE, t1, FOO, BAR);
+  Dbg.dump(CYCLE, "FOO", FOO);
+  Dbg.dump(CYCLE+1, "FOO", FOO, "BAR", BAR);
 
   std::cout << "PASS" << std::endl;
 
