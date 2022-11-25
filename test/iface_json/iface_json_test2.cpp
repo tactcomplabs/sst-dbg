aded0680b75f8e3b3d5cc35d70ffa964dc7d90b4
jleidel <john.leidel@gmail.com>
2022-11-25 11:01:24 -0600

    adding output checker tests

---
+++ test/iface_json/iface_json_test2.cpp
@@ -0,0 +1,31 @@
+//
+// iface_json_test2.cpp
+//
+// Copyright (C) 2017-2022 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+#include <iostream>
+#include <string>
+#include "SSTDebug.h"
+
+std::string TEST_NAME = "iface_json_test2";
+
+int main( int argc, char **argv ){
+  std::cout << "TEST = " << __FILE__ << std::endl;
+
+  SSTDebug Dbg(TEST_NAME,"./");
+
+  if( Dbg.IsJSON() ){
+    std::cout << "PASS" << std::endl;
+    return 0;
+  }else{
+    std::cout << "FAIL" << std::endl;
+    return -1;
+  }
+}
+
+// EOF
---
+++ test/iface_json/iface_json_test3.cpp
@@ -0,0 +1,31 @@
+//
+// iface_json_test3.cpp
+//
+// Copyright (C) 2017-2022 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+#include <iostream>
+#include <string>
+#include "SSTDebug.h"
+
+std::string TEST_NAME = "iface_json_test3";
+
+int main( int argc, char **argv ){
+  std::cout << "TEST = " << __FILE__ << std::endl;
+
+  SSTDebug Dbg(TEST_NAME,"./");
+
+  if( !Dbg.IsASCII() ){
+    std::cout << "PASS" << std::endl;
+    return 0;
+  }else{
+    std::cout << "FAIL" << std::endl;
+    return -1;
+  }
+}
+
+// EOF
