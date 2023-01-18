14f8ba35fbbdf7d2cddea58f7e01351bdfb75d24
jleidel <john.leidel@gmail.com>
2023-01-18 14:13:11 -0600

    adding MPI support for rank-based output; updating copyrights across all files; moving ASCII and JSON data readers to public methods

--- src/console/sst-dbg.cpp
+++ src/console/sst-dbg.cpp
@@ -1,7 +1,7 @@
 //
 // _sst-dbg_cpp_
 //
-// Copyright (C) 2017-2022 Tactical Computing Laboratories, LLC
+// Copyright (C) 2017-2023 Tactical Computing Laboratories, LLC
 // All Rights Reserved
 // contact@tactcomplabs.com
 //
